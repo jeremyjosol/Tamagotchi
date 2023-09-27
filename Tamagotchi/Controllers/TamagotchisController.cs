37adb469440d97c4a694bfd12159a9034b272902
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-27 10:18:21 -0700

    Initialize project + .gitignore

---
+++ Tamagotchi/Controllers/TamagotchisController.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Tamagotchi.Models;
+
+namespace Tamagotchi.Controllers
+{
+  public class TamagotchiController : Controller
+  {
+  }
+}
---
+++ Tamagotchi/Models/Tamagotchi.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Tamagotchi.Models
+{
+  public class Tamagotchis
+  {
+
+  }
+}
