f81443dcfe83f5d58c30d3a9778a203c438ec5a5
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-27 10:26:49 -0700

    Add Tamagotchi class, constructor, and properties
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- Tamagotchi/Controllers/TamagotchisController.cs
+++ Tamagotchi/Controllers/TamagotchisController.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Tamagotchi.Models;
+using TamagotchiToy.Models;
 
 namespace Tamagotchi.Controllers
 {
--- Tamagotchi/Models/Tamagotchi.cs
+++ Tamagotchi/Models/Tamagotchi.cs
@@ -1,9 +1,29 @@
 using System.Collections.Generic;
 
-namespace Tamagotchi.Models
+namespace TamagotchiToy.Models
 {
-  public class Tamagotchis
+  public class Tamagotchi
   {
-
+    public string Name { get; set; }
+    public int Hunger { get; set; }
+    public int Happiness { get; set; }
+    public bool IsAlive { get; set; }
+  
+    public Tamagotchi(string name)
+    {
+      Name = name;
+      Hunger = 25;
+      Happiness = 25;
+      IsAlive = true;
+    }
+    public void Feed()
+    {
+      Hunger += 5;
+      Happiness -= 2;
+    }
+    public void Play()
+    {
+      
+    }
   }
 }
